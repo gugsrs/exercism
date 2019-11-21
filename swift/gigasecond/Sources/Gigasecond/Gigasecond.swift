54ac362af2a64dfae8cf91db8cbb5abb14ca3606
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2019-11-20 23:11:01 -0300

    Use guard instead of force unwrapping variables

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -7,8 +7,10 @@ struct Gigasecond {
 		let dateFormatter = DateFormatter()
 		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
 		dateFormatter.timeZone = TimeZone(identifier: "UTC")
-		var date = dateFormatter.date(from: from)
-		date = date!.addingTimeInterval(1000000000)
-		self.description = dateFormatter.string(from: date!)
+		guard var date = dateFormatter.date(from: from) else {
+			return nil
+		}
+		date = date.addingTimeInterval(1000000000)
+		self.description = dateFormatter.string(from: date)
 	}
 }
