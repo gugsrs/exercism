a57fc9eb0576b51b4071ed30748deba273feda13
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2020-10-01 15:46:14 +0200

    Add raindros exercise

---
+++ bash/raindrops/raindrops.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+main () {
+  raindrops=""
+  if (( $1 % 3 == 0)); then
+    raindrops="Pling"
+  fi
+  if (( $1 % 5 == 0)); then
+    raindrops="${raindrops}Plang"
+  fi
+  if (( $1 % 7 == 0)); then
+    raindrops="${raindrops}Plong"
+  fi
+  if [[ -z "$raindrops" ]]; then
+    echo $1
+  else
+    echo "${raindrops}"
+  fi
+  exit 0
+}
+
+main "$@"
